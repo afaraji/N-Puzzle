f2c4dff0e113a778c68d65a7fb73953dbf9ce92f
afaraji <alegi.bruno1@gmail.com>
2021-12-26 00:08:26 +0100

    working on A*

--- solver.py
+++ solver.py
@@ -1,35 +1,63 @@
-import heapq
+from heapq import heappush, heappop
 from utilities import swap_index
 
 class C_State:
-	def __init__(self, array, string, parent, f_coast):
+	def __init__(self, array, string, parent, level, cost):
 		self.array = array
 		self.string = string
 		self.parent = parent
-		self.f_coast = f_coast
+		self.cost = cost
+		self.level = level
+	def __lt__(self, nxt):
+		return self.cost < nxt.cost
 
-def g(n): # the coast from starting pos to n
-	val = 0
-	return val
+class priority_queue:
+	def __init__(self):
+		self.heap = []
 
-def h(arr, goal, heuristic_type): # heuristic: estimate the coast from the n pos to the tatget
+	# Inserts a new key 'k'
+	def push(self, k):
+		heappush(self.heap, k)
+
+	# Method to remove minimum element
+	# from Priority Queue
+	def pop(self):
+		return heappop(self.heap)
+
+	# Method to know if the Queue is empty
+	def empty(self):
+		if not self.heap:
+			return True
+		else:
+			return False
+
+def g(level): # the cost from starting pos to n
+	return level + 1
+
+def h(arr, goal, heuristic_type) -> int: # heuristic: estimate the cost from the n pos to the tatget
 	if heuristic_type == 1:
-		return heuristic_func1(arr, goal)
+		return my_heuristic(arr, goal)
 	if heuristic_type == 2:
 		return hamming(arr, goal)
 	return manhattan_distance(arr, goal)
 
-def heuristic_func1(arr, goal):
-	return manhattan_distance(arr, goal)
 
-def hamming(arr, goal):
+def my_heuristic(arr, goal) -> int:
+	count = 0
+	for i in range (len(goal)):
+		if (arr[i] != goal[i]):
+			index = arr.index(goal[i])
+			count += abs(index - i)
+	return count
+
+def hamming(arr, goal) -> int:
 	count = 0
 	for i in range(len(goal)):
 		if(arr[i] != goal[i]):
 			count += 1
 	return count
 
-def manhattan_distance(array, goal):
+def manhattan_distance(array, goal) -> int:
 	arr = array[:]
 	count = 0
 	for i in range(len(goal)):
@@ -39,8 +67,8 @@ def manhattan_distance(array, goal):
 			count += 1
 	return count
 
-def f(state, goal, heuristic_type):
-	return g(state.parent.g) + h(state.arr, goal, heuristic_type)
+def f(state, goal, heuristic_type) -> int:
+	return g(state.parent.level) + h(state.arr, goal, heuristic_type)
 
 ################   print   #####################
 
--- test.py
+++ test.py
@@ -8,42 +8,55 @@ import sys
 
 
 
-def read_input_file():
-	if len(sys.argv) != 2:
-		print("python3 nPuszzle.py puzzle.txt")
-		exit(1)
-	f = open(sys.argv[1],'r')
-	lines = f.readlines()
-	f.close()
-	count = 0
-	data = []
-	for line in lines:
-		count += 1
-		line = line.strip()
-		try: hashtag = line.index("#")
-		except: hashtag = -1
-		if (hashtag != -1): line = line[:hashtag]
-		if (len(line) > 0): data.append(line)
+# def read_input_file():
+# 	if len(sys.argv) != 2:
+# 		print("python3 nPuszzle.py puzzle.txt")
+# 		exit(1)
+# 	f = open(sys.argv[1],'r')
+# 	lines = f.readlines()
+# 	f.close()
+# 	count = 0
+# 	data = []
+# 	for line in lines:
+# 		count += 1
+# 		line = line.strip()
+# 		try: hashtag = line.index("#")
+# 		except: hashtag = -1
+# 		if (hashtag != -1): line = line[:hashtag]
+# 		if (len(line) > 0): data.append(line)
 
-	arr = []
-	m_ap = []
+# 	arr = []
+# 	m_ap = []
 
-	try:
-		size = int(data[0])
-		for i in range(1, size + 1):
-			tmp = data[i].split()
-			tmp = [int(i) for i in tmp]
-			m_ap.append(tmp)
-			arr += tmp
-			if (len(m_ap[-1]) != size):
-				raise Exception()
-		if (len(m_ap) != size):
-			raise Exception()
-		return m_ap, arr
-	except:
-		print("Invalid File.")
-		exit(2)
+# 	try:
+# 		size = int(data[0])
+# 		for i in range(1, size + 1):
+# 			tmp = data[i].split()
+# 			tmp = [int(i) for i in tmp]
+# 			m_ap.append(tmp)
+# 			arr += tmp
+# 			if (len(m_ap[-1]) != size):
+# 				raise Exception()
+# 		if (len(m_ap) != size):
+# 			raise Exception()
+# 		return m_ap, arr
+# 	except:
+# 		print("Invalid File.")
+# 		exit(2)
 
-myList, array = read_input_file()
-print(myList)
-print(array)
+# myList, array = read_input_file()
+# print(myList)
+# print(array)
+
+class C_State:
+	def __init__(self, array, string, parent, f_coast):
+		self.array = array
+		self.string = string
+		self.parent = parent
+		self.f_coast = f_coast
+	def print(self):
+		return self.array, self.string
+
+lol = C_State("123", "789", None, 100)
+
+print(lol)
